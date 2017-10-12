889cafb0c6a038f817e95eb6144c9758018844c4
jack <a917106275@163.com>
2017-10-12 21:37:27 +0800

    Add class of LoginActivity.

---
+++ FoodOrderingCustomerApp/app/src/main/java/com/example/admin/myapplication/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.admin.myapplication.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.admin.myapplication.R;
+
+/**
+ * Created by ADMIN on 2017/10/12.
+ */
+
+public class LoginActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
